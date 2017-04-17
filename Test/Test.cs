a484a4d3f8bd32dc2c6e3e74cd05853ab46763cc
Jesper Dahlberg <jesperdahlberg400@hotmail.com>
2017-04-17 23:14:36 +0200

    Test refarcot

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,35 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using Core.Context;
+﻿using System.Collections.Generic;
 using Core.Models;
+using Core.Repositories.Interfaces;
 using MongoDB.Bson;
-using MongoDB.Driver;
 using Moq;
 using Xunit;
+using System.Threading.Tasks;
 
 namespace Test
 {
 	public class Test
-	{		
+	{
 		[Fact]
 		public void WhiskeyCollectionsReturnsResult()
 		{
 			// Arrange
-			var whiskeyCollection = Get();
-			
-			var mock = new Mock<IWhiskeyContext>();
-			mock.Setup(w => w.Whiskeys).Returns(whiskeyCollection);
+			var whiskeys = Get();
+			var repoMock = new Mock<IWhiskeyRepository>();
+			repoMock.Setup(m => m.GetAll()).Returns(whiskeys);
 			
 			// Act
 			// Assert
-			Assert.NotNull(mock.Object.Whiskeys);
+			Assert.NotNull(repoMock.Object);
+		}
+		private Task<IEnumerable<Whiskey>> Get()
+		{
+			return new Task<IEnumerable<Whiskey>>(GetWhiskeys);
 		}
 
-		private IMongoCollection<Whiskey> Get()
+		private IEnumerable<Whiskey> GetWhiskeys()
 		{
-			return null;
+			return new List<Whiskey>
+			{
+				new Whiskey
+				{
+					Age = 12, AlcoholPercentage = 60, Id =  new ObjectId("123"),Name = "Test",Distillery = new Distillery{Name = "TestDist", Description = "Desc"}
+				}
+			};
 		}
 	}
 }
